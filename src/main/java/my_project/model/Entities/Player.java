f702ee42e5a421ba63514d7bd68323c26a1c52cf
Justus <tinnjust@kkg-do.eu>
2025-11-17 08:21:12 +0100

    Entity

--- src/main/java/my_project/model/Entities/Player.java
+++ src/main/java/my_project/model/Entities/Player.java
@@ -3,8 +3,12 @@ package my_project.model.Entities;
 import javax.xml.namespace.QName;
 
 public class Player extends Entity{
+
     public Player(double hp, int speed, double stamina, int defense, String Name) {
         super(hp, speed, stamina, defense, Name);
-
     }
+
+    
+
+
 }
