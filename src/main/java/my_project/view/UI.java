3132a876b492bb6128dbf3bbac65ad7c4412578f
sahibila <sahibila@kkg-do.eu>
2025-11-17 09:30:27 +0100

    .

--- src/main/java/my_project/view/UI.java
+++ src/main/java/my_project/view/UI.java
@@ -13,6 +13,9 @@ import java.awt.event.MouseEvent;
 public class UI extends InteractiveGraphicalObject {
 
     boolean startscreen = true;
+    private int alpha = 255;
+    private double alphaTimer = 0;
+
     public UI() {
 
     }
@@ -23,11 +26,28 @@ public class UI extends InteractiveGraphicalObject {
 
     public void drawStartScreen(DrawTool drawTool){
 
-        drawTool.setCurrentColor(Color.BLACK);
+        drawTool.setCurrentColor(0, 0, 0, 255);
         drawTool.drawFilledRectangle(0,0, 1920, 1080);
+        drawTool.setCurrentColor(255,192,203, alpha);
+        drawTool.formatText("Algerian", 2, 200);
+        drawTool.drawText(Config.WINDOW_WIDTH/2-300, Config.WINDOW_HEIGHT/2-150, "JOJO'S");
+        drawTool.setCurrentColor(255, 255, 130, alpha);
+        drawTool.drawText(Config.WINDOW_WIDTH/2-450, Config.WINDOW_HEIGHT/2+30, "CRAWLER");
+        drawTool.formatText("Algerian", 4, 50);
         drawTool.setCurrentColor(Color.WHITE);
-        drawTool.formatText("Algerian", 2, 300);
-        drawTool.drawText(Config.WINDOW_WIDTH/2-450, Config.WINDOW_HEIGHT/2-150, "START");
+        drawTool.drawText(Config.WINDOW_WIDTH/2-200, Config.WINDOW_HEIGHT/2+350, "Click to Start");
+    }
+
+    @Override
+    public void update(double dt){
+        alphaTimer += dt;
+
+        if (alphaTimer > 0 && alphaTimer < 5) {
+            alpha -= 10*dt;
+        }
+        if (alphaTimer > 5){
+            alpha += 20*dt;
+        }
     }
 
     @Override
