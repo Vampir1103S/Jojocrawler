56c7d47bb3294292a4acff3ad83f3ecb86387024
sahibila <sahibila@kkg-do.eu>
2025-11-17 08:26:30 +0100

    .

--- src/main/java/my_project/view/UI.java
+++ src/main/java/my_project/view/UI.java
@@ -1,15 +1,17 @@
 package my_project.view;
 
 import KAGO_framework.model.GraphicalObject;
+import KAGO_framework.model.InteractiveGraphicalObject;
 import KAGO_framework.view.DrawTool;
 import my_project.Config;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
 
 
-public class UI extends GraphicalObject {
+public class UI extends InteractiveGraphicalObject {
 
-
+    boolean startscreen = true;
     public UI() {
 
     }
@@ -18,13 +20,23 @@ public class UI extends GraphicalObject {
         drawStartScreen(drawTool);
     }
     public void drawStartScreen(DrawTool drawTool){
-        drawTool.setCurrentColor(Color.BLACK);
-        drawTool.drawFilledRectangle(0,0, 1920, 1080);
-        drawTool.setCurrentColor(Color.WHITE);
-        drawTool.formatText("Algerian", 1, 300);
-        drawTool.drawText(Config.WINDOW_WIDTH/2-450, Config.WINDOW_HEIGHT/2-150, "START");
+        if (startscreen){
+            drawTool.setCurrentColor(Color.BLACK);
+            drawTool.drawFilledRectangle(0,0, 1920, 1080);
+            drawTool.setCurrentColor(Color.WHITE);
+            drawTool.formatText("Algerian", 4, 300);
+            drawTool.drawText(Config.WINDOW_WIDTH/2-450, Config.WINDOW_HEIGHT/2-150, "START");
+        }
     }
 
-
-
+    @Override
+    public void mouseClicked(MouseEvent e){
+        if (e.getButton() == MouseEvent.BUTTON1) {
+            if (startscreen) {
+                startscreen = false;
+                System.out.println("startscreen");
+            }
+        }
+    }
 }
+
