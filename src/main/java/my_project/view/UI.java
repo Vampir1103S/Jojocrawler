de0761d17635dd48f10f3a5584b9c2d8e4eed34b
sahibila <sahibila@kkg-do.eu>
2025-11-19 12:43:45 +0100

    UI Animation fertig

--- src/main/java/my_project/view/UI.java
+++ src/main/java/my_project/view/UI.java
@@ -13,8 +13,12 @@ import java.awt.event.MouseEvent;
 public class UI extends InteractiveGraphicalObject {
 
     boolean startscreen = true;
-    private int alpha = 255;
+    private double alpha = 255;
     private double alphaTimer = 0;
+    private double alphaAnimation = 255;
+    private double xPos = Config.WINDOW_WIDTH/2;
+    private double yPos = Config.WINDOW_HEIGHT/2;
+
 
     public UI() {
 
@@ -26,36 +30,50 @@ public class UI extends InteractiveGraphicalObject {
 
     public void drawStartScreen(DrawTool drawTool){
 
-        drawTool.setCurrentColor(0, 0, 0, 255);
+        drawTool.setCurrentColor(0, 0, 0, (int) alphaAnimation);
         drawTool.drawFilledRectangle(0,0, 1920, 1080);
-        drawTool.setCurrentColor(255,192,203, alpha);
+        drawTool.setCurrentColor(255,192,203, (int) alpha);
         drawTool.formatText("Algerian", 2, 200);
-        drawTool.drawText(Config.WINDOW_WIDTH/2-300, Config.WINDOW_HEIGHT/2-150, "JOJO'S");
-        drawTool.setCurrentColor(255, 255, 130, alpha);
-        drawTool.drawText(Config.WINDOW_WIDTH/2-450, Config.WINDOW_HEIGHT/2+30, "CRAWLER");
+        drawTool.drawText(xPos-300, yPos-150, "JOJO'S");
+        drawTool.setCurrentColor(255, 255, 130, (int) alpha);
+        drawTool.drawText(xPos-450, yPos+30, "CRAWLER");
         drawTool.formatText("Algerian", 4, 50);
         drawTool.setCurrentColor(Color.WHITE);
-        drawTool.drawText(Config.WINDOW_WIDTH/2-200, Config.WINDOW_HEIGHT/2+350, "Click to Start");
+        drawTool.drawText(xPos-200, yPos+350, "Click to Start");
     }
 
     @Override
     public void update(double dt){
         alphaTimer += dt;
 
-        if (alphaTimer > 0 && alphaTimer < 5) {
-            alpha -= 10*dt;
+        if (alphaTimer < 5) {
+            alpha -= 21*dt;
         }
-        if (alphaTimer > 5){
-            alpha += 20*dt;
+        if (alphaTimer >= 5) {
+           alpha += 20*dt;
+        }
+        if (alphaTimer >= 10) {
+            alphaTimer = 0;
+        }
+
+        if (startscreen == false) {
+            startScreenAn(dt);
         }
     }
 
     @Override
     public void mouseClicked(MouseEvent e){
         if (e.getButton() == MouseEvent.BUTTON1) {
+            startscreen = false;
+        }
+    }
 
-            System.out.println(e.getButton());
+    public void startScreenAn(double dt){
+        yPos -= 850*dt;
+        alphaAnimation -= 220*dt;
+        if (yPos < -400){
             Controller.switchScene(1);
+            alphaAnimation = 0;
         }
     }
 }
